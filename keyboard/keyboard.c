90c8c8a2af98abde607d830cb9614c834c7381c6
Yuval Adam <yuval@y3xz.com>
2016-03-25 23:29:50 +0300

    Initial USB handling code

--- keyboard/keyboard.c
+++ keyboard/keyboard.c
@@ -1,14 +1,35 @@
-#include <avr/eeprom.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+
+#include "usbdrv.h"
+
 #define F_CPU 16500000L  // 16.5 MHZ
 #include <util/delay.h>
 
+USB_PUBLIC uchar usbFunctionSetup(uchar data[8]) {
+	return 0; // do nothing for now
+}
+
 int main() {
-	DDRB |= _BV(DDB1);
-	while (1) {
-		PORTB ^= _BV(PB1);
-		_delay_ms(100);
-		PORTB ^= _BV(PB1);
-		_delay_ms(900);
+	uchar i;
+	wdt_enable(WDT0_1S);
+
+	usbInit();
+
+	usbDeviceDisconnect();
+	for (i=0; i<250; i++) {
+		wdt_reset();
+		_delay_ms(2);
 	}
+	usbDeviceConnect();
+
+	sei();
+
+	while (1) {
+		wdt_reset();
+		usbPoll();
+	}
+
+	return 0;
 }
