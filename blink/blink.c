46db120e2722b41b10b9fa78fca5f669bfc8fd47
Yuval Adam <yuval@y3xz.com>
2016-03-25 20:49:56 +0300

    Initial udev rules and blink project

---
+++ blink/blink.c
@@ -0,0 +1,16 @@
+
+#include <avr/eeprom.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main() {
+	DDRB |= _BV(DDB1);
+	while (1) {
+		PORTB ^= _BV(PB1);
+		_delay_ms(100);
+		PORTB ^= _BV(PB1);
+		_delay_ms(900);
+	}
+}
+
