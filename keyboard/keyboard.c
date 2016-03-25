77ceeed38242344612b7b8cba13c6832b61bc15c
Yuval Adam <yuval@y3xz.com>
2016-03-25 23:07:12 +0300

    Initial Makefile and keyboard.c

---
+++ keyboard/keyboard.c
@@ -0,0 +1,14 @@
+#include <avr/eeprom.h>
+#include <avr/io.h>
+#define F_CPU 16500000L  // 16.5 MHZ
+#include <util/delay.h>
+
+int main() {
+	DDRB |= _BV(DDB1);
+	while (1) {
+		PORTB ^= _BV(PB1);
+		_delay_ms(100);
+		PORTB ^= _BV(PB1);
+		_delay_ms(900);
+	}
+}
